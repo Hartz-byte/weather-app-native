6ac57224ba9c3a4e00f31b836d4b2ec0c0898e0d
Hartz-byte <harshmail281199@gmail.com>
2023-12-13 12:53:11 +0530

    first commit

---
+++ Components/Screens/Home.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import React from "react";
+import { deviceHeight, deviceWidth } from "../Domensions/Dimensions";
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const Home = () => {
+  return (
+    <View>
+      <ImageBackground
+        source={require("../../assets/images/image2.jpg")}
+        style={{ height: deviceHeight, width: deviceHeight }}
+        imageStyle={{ opacity: 0.6, backgroundColor: "black" }}
+      />
+
+      <View style={{ position: 'absolute' }}>
+        <View>
+            <Icon name='menu' size={46} color='white' />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
